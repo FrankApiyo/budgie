8d701da2b5ec6e3421aaf673467b6b65f1b90f49
FrankApiyo <franklineapiyo@gmail.com>
2025-03-06 16:23:23 +0300

    feat: Add a way to update username in user_settings_live

--- lib/budgie/accounts/user.ex
+++ lib/budgie/accounts/user.ex
@@ -111,6 +111,15 @@ defmodule Budgie.Accounts.User do
   end
 
   @doc """
+  A user changeset for changing their name.
+  """
+  def name_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name])
+    |> validate_name()
+  end
+
+  @doc """
   A user changeset for changing the password.
 
   ## Options
